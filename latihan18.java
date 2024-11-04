7770ab0e8962e2299b5672381195477024f43c65
Bara Abityyi <122656775+BaraAbiyyi@users.noreply.github.com>
2024-11-04 10:22:33 +0700

    Add files via upload

---
+++ latihan18.java
@@ -0,0 +1,16 @@
+package pertemuan5;
+
+public class latihan18 {
+    public static void main(String[] args) {
+        // Mulai dari 3.5, kelipatan 3.5
+        double angka = 3.5;
+        
+        // Looping dari 3.5 sampai dengan 35.0
+        while (angka <= 35.0) {
+            System.out.println(angka);
+            angka += 3.5; // Tambahkan 3.5 setiap iterasi
+        }
+    }
+}
+
+
